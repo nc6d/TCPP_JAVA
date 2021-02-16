4a74e3d2ede8a0b485f1bd23e43e3f6238e3ba25
nc6d <6od92002@gmail.com>
2021-02-16 20:03:24 +0200

    Зміни_2

--- src/TCPP_pckg/Main.java
+++ src/TCPP_pckg/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("I love DonNU ");
         System.out.println("I love Computer Science! ");
+        System.out.println("Change 2 ALFA ");
     }
 }
