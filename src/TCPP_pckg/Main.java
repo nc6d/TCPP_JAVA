f1432331d7d2304b1ea911db4b4675509971e112
Bohdan Opanasyuk <58623074+nc6d@users.noreply.github.com>
2021-02-16 22:39:28 +0200

    Зміни з браузера

--- src/TCPP_pckg/Main.java
+++ src/TCPP_pckg/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("I love DonNU ");
         System.out.println("I love Computer Science! ");
         System.out.println("Change 2 ALFA ");
+        System.out.println("Change from browser ");
     }
 }
