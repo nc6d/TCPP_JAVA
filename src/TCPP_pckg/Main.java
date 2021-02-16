ae7641231c91c573913499b4c67cd2f1cd961e7e
nc6d <6od92002@gmail.com>
2021-02-16 17:27:16 +0200

    Зміни_1

--- src/TCPP_pckg/Main.java
+++ src/TCPP_pckg/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("I love DonNU ");
-        System.out.println("I love Computer Science ");
+        System.out.println("I love Computer Science! ");
     }
 }
